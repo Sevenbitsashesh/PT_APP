45a18d608cf31049615aa4e04b88f4e6b8ba5631
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-04-22 19:37:59 +0530

    PT_APP_TrainerServices_22_03_2019

--- src/providers/client/client.ts
+++ src/providers/client/client.ts
@@ -10,13 +10,12 @@ export class ClientProvider {
   constructor(public http: HttpClient) {
     
         this.header.append("Authorization","Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVjNWE3YTg5ZmI2ZmMwNmY0ZjU2ODBiMyIsImVtYWlsIjoicGFzaGVzaEBnbWFpbC5jb20iLCJleHAiOjE1NTU0MjE3MzgsImlhdCI6MTU1NDgxNjkzOH0.bPDtErUgzgUtBVZ-5mYG2Re6jghQ7P3Ie2LFk5x-wqI")
-        this.header.append("Content-Type","application/x-www-form-urlencoded");
-        this.header.append("Accept","application/json");
+        
   }
-  addClient(clientModel): Observable<any> {
+  addClient(clientModel,user): Observable<any> {
     
     
-    return this.http.post(API_URL+'client/addclient',clientModel, {headers: this.header});
+    return this.http.post(API_URL+'client/addclient',clientModel, {headers: {"Content-Type": "application/x-www-form-urlencoded","Accept": "application/json","Authorization": "Bearer "+user.token}});
   }
   getMyClients(authdetail,user): Observable<any> {
 
