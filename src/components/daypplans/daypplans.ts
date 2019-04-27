1a023420d6f952f0f3fb6cba4140f01d9a6f3341
Sevenbitsashesh <parmar.ashesh@tristonsoft.com>
2019-04-27 19:30:33 +0530

    PT_App_MealPlan_27-04-2019

--- src/components/daypplans/daypplans.ts
+++ src/components/daypplans/daypplans.ts
@@ -3,7 +3,12 @@ import { FoodProvider } from '../../providers/food/food';
 import { debounce } from 'ionic-angular/umd/util/util';
 import { debounceTime } from 'rxjs/operators/debounceTime';
 import { Observable } from 'rxjs';
+export class FoodModel {
+  foodid: String;
+  foodname: String;
+  nutrients: String;
 
+}
 @Component({
   selector: 'daypplans',
   templateUrl: 'daypplans.html'
@@ -19,47 +24,52 @@ export class DaypplansComponent implements AfterViewInit {
   foodEveSnacks = [];
   foodDinner = [];
   foodBefSleep = [];
+  totalBreakfastNutri: number = 0;
   constructor(private foodService: FoodProvider) {
     
   }
   ngAfterViewInit() {
-    const tar = document.getElementById('searchInput');
-    
-    const inptSearch =  Observable.fromEvent(tar,'keyup').map(x => x).debounceTime(1000).take(1);
-        inptSearch.subscribe(data => this.getItem(data));
+    // const tar = document.getElementById('searchInput');    
+    // const inptSearch =  Observable.fromEvent(tar,'keyup').map(x => x).debounceTime(1000).take(1);
+    //     inptSearch.subscribe(data => this.getItem(data).subscribe(data => {
+    //       this.foodBreakfast = data;
+    //     }));
   }
   doInfinite(infiniteScroll) {
 
   }
   getItems(name) {
     // name = "45242685";
+    console.log(name.target.value);
     
-    
-    // if(name.target.value) {
-      
+    if(name.target.value) {
+      this.foodService.getFoodByName(name).subscribe(foodData => {
+        console.log(foodData['parsed'].length);
+        if(foodData['parsed'].length > 0) {
+          this.searchItems = foodData['parsed'];          
+        }
+        else {
+          this.searchItems = [];
+        }
+        
+        
+        
+      });
 
-        // }    
+        }    
   }
-  getItem(name) {
-    
-    this.foodService.getFoodByName(name).subscribe(data => {
-    
-      // this.searchItems = data['list']['item'];
-      // this.searchItems.slice()
-      this.searchItems = data['parsed'];
-      console.log(this.searchItems);
-  })
-  }
-
+  
   clicked(event) {
     console.log(event);
   }
-  addfoodBreakfast(ndbno) {
+  addfoodBreakfast(food) {
     
-    this.foodService.getFoodNutrients(ndbno).subscribe(data => {
-       this.foodBreakfast.push(data['report']['food']);
-       console.log(data['report']['food']);
-      })
+    // this.foodService.getFoodNutrients(food).subscribe(data => {
+       this.foodBreakfast.push(food);
+       console.log(this.foodBreakfast);
+       this.totalBreakfastNutri = (this.totalBreakfastNutri + food.nutrients.ENERC_KCAL);
+       console.log(this.totalBreakfastNutri);
+      // })
   }
   addfoodMorningSnacks(ndbno) {
     
